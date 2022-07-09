0fdb94300834c58d8543b18d712bff1ca0007736
Celeste Robinson <celestekrobinson@gmail.com>
2022-07-08 20:46:43 -0600

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import CategoryMenu from './components/categoryMenu/CategoryMenu';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home';
+import Navigation from './routes/navigation/Navigation.jsx';
+
 const App = () => {
-  return <CategoryMenu />;
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/home/home.jsx
@@ -0,0 +1,7 @@
+import CategoryMenu from '../../components/categoryMenu/CategoryMenu';
+
+const Home = () => {
+  return <CategoryMenu />;
+};
+
+export default Home;
