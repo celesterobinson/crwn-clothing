2ab5ef53fe6acee5ff946477f6fed3571618302e
Celeste Robinson <celestekrobinson@gmail.com>
2022-07-09 17:35:54 -0600

    created sign up, form input, and button components

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,18 @@
+import './Button.scss';
+
+const BUTTON_TYPE_CLASSES = {
+  google: 'google-sign-in',
+  inverted: 'inverted',
+};
+const Button = ({ children, buttonType, otherProps }) => {
+  return (
+    <button
+      className={`${BUTTON_TYPE_CLASSES[buttonType] || ''} button-container`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
